f3983a136afa11e7d6af4dfbc01079a2f4475a3a
kabamehmetali <mehmetalikabaa@icloud.com>
2025-02-16 15:42:58 -0500

    last update

--- frontend/src/services/apollo.js
+++ frontend/src/services/apollo.js
@@ -3,9 +3,8 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || '/graphql'
+  uri: 'http://localhost:4000/graphql'
 });
-
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('token');
